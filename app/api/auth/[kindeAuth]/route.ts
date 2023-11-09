c4bb4e59ee2469875c0ee259b341b60fc60e595f
jatin <jatin.ahluwalia.5@gmail.com>
2023-11-09 18:15:29 +0530

    initial

---
+++ app/api/auth/[kindeAuth]/route.ts
@@ -0,0 +1,7 @@
+import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextApiRequest } from "next";
+
+export async function GET(request: NextApiRequest, { params }: any) {
+  const endpoint = params.kindeAuth;
+  return handleAuth(request, endpoint);
+}
