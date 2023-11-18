cfe3c7e3f0a07daae2ee1f25703212500361af09
jatin <jatin.ahluwalia.5@gmail.com>
2023-11-19 01:14:52 +0530

    auth fix

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,7 @@
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export const GET = async (request: NextRequest, { params }: any) => {
   const endpoint = params.kindeAuth;
-  return handleAuth(request, endpoint);
-}
+  return handleAuth(request, endpoint) as Promise<NextResponse>;
+};
